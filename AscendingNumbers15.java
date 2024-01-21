289c5ae8475b0da014734ac8553f9bf7da356dcc
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2024-01-20 19:45:05 -0800

    Add files via upload

---
+++ AscendingNumbers15.java
@@ -0,0 +1,8 @@
+public class AscendingNumbers15 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("AscendingNumber: " + i);
+            System.out.println("This maybe makes a little sense");
+        }
+    }
+}
