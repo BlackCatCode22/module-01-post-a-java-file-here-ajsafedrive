8d44250d82686684ab086b507d20460cd5c8d60e
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2024-01-20 19:38:03 -0800

    Add files via upload

---
+++ WhoAmI.java
@@ -0,0 +1,8 @@
+
+        public class WhoAmI {
+            public static void main(String[] args) {
+                String name = "Anthony";
+                System.out.println("Who Am I? " + name + " Like I said in my first commit ever introducing myself.");
+                System.out.println("I'm here and equal parts excited and nervous! It took so many tries to get this to actually run lol");
+            }
+        }
