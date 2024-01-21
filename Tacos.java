12f9f0836c5abbdbb6cc288aab0a7ad074aaaf61
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2024-01-20 19:40:58 -0800

    Add files via upload

---
+++ Tacos.java
@@ -0,0 +1,8 @@
+public class Tacos {
+    public static void main(String[] args) {
+        String name = "Anthony";
+        int tacos = 2;
+
+        System.out.println(name + " + " + tacos + " tacos makes me happy");
+    }
+}
