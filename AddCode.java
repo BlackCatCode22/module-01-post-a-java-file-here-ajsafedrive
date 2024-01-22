c71e13363a4a4c551ab2ce99b22561c45a5277f6
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2024-01-21 19:30:54 -0800

    Add files via upload

---
+++ AddCode.java
@@ -0,0 +1,9 @@
+public class AddCode {
+    public static void main(String[] args) {
+        // What is the sum of 5 (number1) and 10 (number2)??
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
